53d07660e2f6c316b9086b76bbd19bf1178d8321
StephanieMfon <108840244+StephanieMfon@users.noreply.github.com>
2023-01-10 04:11:52 +0100

    added full page styling

--- SCRIPTS/script1.js
+++ SCRIPTS/script1.js
@@ -86,11 +86,11 @@ function toggle(e){
    this.closest('.has-child').classList.toggle('expand')
 }
 
+
 // ======automatic slideshow
 let index = 0;
 const slides = document.querySelectorAll(".slides");
 const dots = document.querySelectorAll(".dot");
-
 function changeSlide(){
 
   number = slides.length - 1
@@ -113,11 +113,48 @@ function changeSlide(){
   dots[index].classList.add("active");
   
   index++;
-  
-  setTimeout(changeSlide,4000);
+  setTimeout(changeSlide,3000);
   
 }
-
 changeSlide();
 
 
+// practicing countdown feature delete when done
+
+const countdown = () => {
+  const countDate = new Date("January 30, 2023 00:00:00").getTime();
+  console.log(countDate)
+  // console.log(countDate)
+  // we can use the get time method to convert it milisecond 
+  // the logic is getting the difference between the future and now and subtrancting it to get our countdown date 
+
+  const now = new Date().getTime();
+console.log(now)
+  // console.log(now)
+  // the new date constructor by itself will always get our current time
+
+  // to get the difference
+
+  const gap = countDate - now;
+  console.log(gap)
+  // now we figure out how time works
+
+  const second = 1000
+  const minuite = second * 60
+  const hour = minuite  * 60
+  const day = hour * 24
+
+// now we calculate from the gap how many days are there
+
+const textDay = Math.floor(gap / day)
+const textHour = Math.floor(gap % day / hour)
+const textMunuite = Math.floor(gap % hour / minuite)
+const textSecond = Math.floor(gap % minuite / second)
+
+document.querySelector('.days').innerText = textDay
+document.querySelector('.hours').innerText = textHour
+document.querySelector('.minuites').innerText = textMunuite
+document.querySelector('.seconds').innerText = textSecond
+
+}
+setInterval(countdown, 1000)
